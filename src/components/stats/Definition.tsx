6cd621b995a0fb8c7b4a8865f2d02825476630e3
Bernardus Jansen <Bernardus.Jansen@tudelft.nl>
2022-02-12 20:11:45 +0100

    iets netter

--- src/components/stats/Definition.tsx
+++ src/components/stats/Definition.tsx
@@ -7,17 +7,15 @@ type Props = {
 export const Definition = ({ geefWurd, definition, loading }: Props) => {
   return (
     <div>
-      {loading && <div>Oersetting laden</div>}
+      {loading && <h5>Oersetting laden...</h5>}
       {!loading && (
         <>
-          <div>
             <h5>
               {Object.values(geefWurd)[0].toString().charAt(0).toUpperCase() +
                 Object.values(geefWurd)[0].toString().substring(1) +
                 ':'}
             </h5>
-          </div>
-          <span className="text-lg font-small text-gray-900 dark:text-gray-100">
+          <span className="font-large text-gray-900 dark:text-gray-100">
             {typeof Object.values(definition)[1] === 'string' &&
               Object.values(definition)[1]}
             {typeof Object.values(definition)[1] === 'object' &&
