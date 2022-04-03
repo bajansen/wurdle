63423ac763b4a5907fdabc179421b60b28af0480
Bernardus Jansen <Bernardus.Jansen@tudelft.nl>
2022-04-03 23:13:21 +0200

    Hack voor lokale tijd

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -15,7 +15,8 @@ export const isWinningWord = (word: string) => {
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = 1642377600000
-  const now = Date.now()
+  // GMT +2 offset
+  const now = Date.now() + 7200000
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
